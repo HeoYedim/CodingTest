00e9dd71255d89a233b46282510554bf574f58e5
HeoYedim <83280164+HeoYedim@users.noreply.github.com>
2023-07-17 17:42:54 +0900

    [level 1] Title: 시저 암호, Time: 3.86 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
@@ -2,12 +2,12 @@ def solution(s, n):
     answer = ''
     
     for i in s:
-        if i != ' ' and i.islower():
+        if i.islower():
             if ord(i)+n>122:
                 answer+=chr(ord(i)+n-26)
             else:
                 answer+=chr(ord(i)+n)
-        elif i != '' and i.isupper():
+        elif i.isupper():
             if ord(i)+n>90:
                 answer+=chr(ord(i)+n-26)
             else:
