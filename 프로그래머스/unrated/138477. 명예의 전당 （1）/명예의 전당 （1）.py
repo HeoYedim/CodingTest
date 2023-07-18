029d0028d59f8736d9ad3de16d7ee7f78f207cc1
HeoYedim <83280164+HeoYedim@users.noreply.github.com>
2023-07-18 15:44:16 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,17 +1,12 @@
 def solution(k, score):
     answer = []
-    last = 0
     honor = []
     
     for i in score:
-        if len(honor) < k:
-            honor.append(i)
-        else:
-            if min(honor) <= i:
-                honor.remove(min(honor))
-                honor.append(i)
-                
-        last = min(honor)
-        answer.append(last)
+        honor.append(i)
+        if len(honor) > k:
+            honor.remove(min(honor))
+            
+        answer.append(min(honor))
         
     return answer
