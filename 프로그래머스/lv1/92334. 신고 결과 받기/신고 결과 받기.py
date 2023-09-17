53e80dafed0ca6612be1efa7cc2db99bc97c4e29
HeoYedim <83280164+HeoYedim@users.noreply.github.com>
2023-09-17 18:26:58 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,33 +1,13 @@
 def solution(id_list, report, k):
-    answer = []
-    report = set(report)
-    report_dict = {}
-    report_values = []
-    report_k = []
+    report=set(report)
+    mail_cnt={x: 0 for x in id_list}
+    report_dict = {x: 0 for x in id_list}
     
-    # report_dict={"frodo":["neo"],"apeach":["muzi","frodo"],"muzi":["neo","frodo"]}
-    for i in report:
-        id = i.split(" ")
-        if id[0] in report_dict:
-            report_dict[id[0]]+=[id[1]]
-        else:
-            report_dict[id[0]]=[id[1]]
-            
-    # report_values=["neo","frodo","frodo","muzi","neo"]
-    for j in report_dict.values():
-        report_values += j
+    for x in report:
+        report_dict[x.split()[1]]+=1
         
-    # 정지된 유저 및 횟수 리스트 ["frodo", "neo"]
-    for c in id_list:
-        if report_values.count(c)>=k:
-            report_k+=[c]
+    for x in report:
+        if report_dict[x.split()[1]] >= k:
+            mail_cnt[x.split()[0]] +=1
             
-    for user in id_list:
-        cnt=0
-        if user in report_dict.keys():
-            for i in report_dict[user]:
-                if i in report_k:
-                    cnt+=1
-        answer+=[cnt]
-            
-    return answer
+    return list(mail_cnt.values())
