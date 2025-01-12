31e5475fa266e09ed5b7fd2b24a057e2edfe55d7
HeoYedim <83280164+HeoYedim@users.noreply.github.com>
2025-01-12 23:06:28 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.92 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,44 +1,32 @@
 def solution(numbers, hand):
     answer = ''
     
-    xy = { 1 : [0, 0], 2 : [0, 1], 3 : [0, 2],
-          4 : [1, 0], 5 : [1, 1], 6 : [1, 2],
-          7 : [2, 0], 8 : [2, 1], 9 : [2, 2],
-          '*' : [3, 0], 0 : [3, 1], '#' : [3, 2]}
+    num_dict = {'*' : 10, "0" : 11, '#' : 12}
     
-    LEFT = xy['*']
-    RIGHT = xy['#']
+    LEFT = num_dict('*')
+    RIGHT = num_dict('#')
     
-    for i in numbers:
-        num = xy[i]
-        
-        if i in [1, 4, 7]:
-            answer += 'L'
+    for num in numbers:
+        if str(num) in ["1", "4", "7", '*']:
+            answer+="L"
             LEFT = num
-        elif i in [3, 6, 9]:
-            answer += 'R'
+        elif str(num) in ["3", "6", "9", '#']:
+            answer+="R"
             RIGHT = num
-            
-        else:
-            LEFT_d = 0
-            RIGHT_d = 0
-            
-            for a, b, c in zip(LEFT, RIGHT, num):
-                LEFT_d += abs(a-c)
-                RIGHT_d += abs(b-c)
-            
-            if LEFT_d < RIGHT_d:
-                answer += 'L'
+        
+        if str(num) in ["2", "5", "8", "0"]:
+            if (abs(int(LEFT) - int(num)) // 3) + (abs(int(LEFT) - int(num)) % 3) < (abs(int(RIGHT) - int(num)) // 3) + (abs(int(RIGHT) - int(num)) % 3):
+                answer += "L"
                 LEFT = num
-            elif LEFT_d > RIGHT_d:
-                answer += 'R'
+            elif (abs(int(LEFT) - int(num)) // 3) + (abs(int(LEFT) - int(num)) % 3) > (abs(int(RIGHT) - int(num)) // 3) + (abs(int(RIGHT) - int(num)) % 3):
+                answer += "R"
                 RIGHT = num
-            else:
-                if hand == 'left':
-                    answer += 'L'
-                    LEFT = num
-                else:
+            else :
+                if hand == "right" :
                     answer += "R"
                     RIGHT = num
+                else:
+                    answer += "L"
+                    LEFT = num
                     
     return answer
